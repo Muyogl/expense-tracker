e113a03f53ecc7937d34f94bb5822dbfca1974ec
Muyogl <Muhammadhaidar54321@gmail.com>
2025-07-04 13:30:22 +0200

    rm: test get

--- server/app.ts
+++ server/app.ts
@@ -1,17 +1,16 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expensesRoute } from "./routes/expense";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
 // Middleware to log requests
 app.use("*", logger());
 
-app.get("/test", (c) =>
-  c.json({
-    message: "Hello, world!",
-  })
-);
-
 app.route("api/expenses", expensesRoute);
+
+app.get("*", serveStatic({ root: "./frontend/dist" }));
+app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
+
 export default app;
